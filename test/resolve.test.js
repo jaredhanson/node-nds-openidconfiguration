2176bbd28c4e4099701edcb64297aa591a9ecff7
Jared Hanson <jaredhanson@gmail.com>
2014-02-08 15:24:48 -0800

    Add test case for resolving metadata.

--- test/resolve.test.js
+++ test/resolve.test.js
@@ -12,4 +12,38 @@ describe('resolve', function() {
     expect(resolve).to.be.a('function');
   });
   
+  describe('resolving metadata', function() {
+    // ** MOCKS **
+    var request = function(options, cb) {
+      expect(options.url).to.equal('https://server.example.com/.well-known/openid-configuration');
+      expect(options.headers['Accept']).to.equal('application/json');
+        
+      process.nextTick(function() {
+        // http://openid.net/specs/openid-connect-discovery-1_0.html#ProviderConfigurationResponse
+        var keys = fs.readFileSync(path.resolve(__dirname, 'data/configuration-draft21.json'), 'utf8');
+        return cb(null, { statusCode: 200 }, keys);
+      });
+    };
+    
+    
+    var setup = $require(MODULE_PATH, { request: request });
+    var resolve = setup();
+    var provider;
+    
+    before(function(done) {
+      resolve('https://server.example.com', function(err, p) {
+        if (err) { return done(err); }
+        provider = p;
+        done();
+      });
+    });
+    
+    it('should resolve metadata', function() {
+      expect(provider).to.be.an('object');
+      expect(provider.id).to.equal('https://server.example.com');
+      expect(provider.issuer).to.equal('https://server.example.com');
+      expect(provider.jwksUrl).to.equal('https://server.example.com/jwks.json');
+    });
+  });
+  
 });
