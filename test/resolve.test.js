85408b041084a2c639818e0cec20ee754d6f0054
Jared Hanson <jaredhanson@gmail.com>
2014-02-08 15:38:21 -0800

    Test cases for error handling.

--- test/resolve.test.js
+++ test/resolve.test.js
@@ -74,4 +74,72 @@ describe('resolve', function() {
     });
   });
   
+  describe('handling unexpected status', function() {
+    // ** MOCKS **
+    var request = function(options, cb) {
+      expect(options.url).to.equal('https://server.example.com/.well-known/openid-configuration');
+        
+      process.nextTick(function() {
+        return cb(null, { statusCode: 500 }, 'Internal server error');
+      });
+    };
+    
+    
+    var setup = $require(MODULE_PATH, { request: request });
+    var resolve = setup();
+    var error, provider;
+    
+    before(function(done) {
+      resolve('https://server.example.com', function(err, p) {
+        error = err;
+        provider = p;
+        done();
+      });
+    });
+    
+    it('should error', function() {
+      expect(error).to.be.an.instanceOf(Error);
+      expect(error.message).to.equal('Unexpected status 500 from https://server.example.com/.well-known/openid-configuration');
+      expect(error.status).to.be.undefined;
+    });
+    
+    it('should not resolve metadata', function() {
+      expect(provider).to.be.undefined;
+    });
+  });
+  
+  describe('handling unparsable body', function() {
+    // ** MOCKS **
+    var request = function(options, cb) {
+      expect(options.url).to.equal('https://server.example.com/.well-known/openid-configuration');
+        
+      process.nextTick(function() {
+        return cb(null, { statusCode: 200 }, '<xml></xml>');
+      });
+    };
+    
+    
+    var setup = $require(MODULE_PATH, { request: request });
+    var resolve = setup();
+    var error, provider;
+    
+    before(function(done) {
+      resolve('https://server.example.com', function(err, p) {
+        error = err;
+        provider = p;
+        done();
+      });
+    });
+    
+    it('should error', function() {
+      expect(error).to.be.an.instanceOf(Error);
+      expect(error.message).to.equal('Failed to parse OpenID provider configuration from https://server.example.com/.well-known/openid-configuration');
+      expect(error.status).to.be.undefined;
+    });
+    
+    it('should not resolve metadata', function() {
+      expect(provider).to.be.undefined;
+    });
+  });
+  
 });
